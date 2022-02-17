34aed4e3ee17b4ce9690b589d8463a24569aca75
bedar89 <bedar89@gmail.com>
2022-02-17 13:46:43 +0000

    function so you don't lose glasses/hat when hit

---
+++ client/dontloseprops.lua
@@ -0,0 +1,17 @@
+Citizen.CreateThread(function ()
+    SetPedCanLosePropsOnDamage(PlayerPedId(), false, 0)
+end)
+
+--Better example
+local lastped = nil
+
+Citizen.CreateThread(function ()
+    while true do
+        if PlayerPedId() ~= lastped then
+            lastped = PlayerPedId()
+            SetPedCanLosePropsOnDamage(PlayerPedId(), false, 0)
+        end
+        Wait(100)
+    end
+end
+)
