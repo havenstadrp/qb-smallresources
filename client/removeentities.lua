f97397ca40fd958c1d3566d3c80c878dc96c824f
bedar89 <bedar89@gmail.com>
2022-06-30 11:52:25 +0000

    removed default blackjack tables removed roulette table

--- client/removeentities.lua
+++ client/removeentities.lua
@@ -1,6 +1,14 @@
 local objects = {
     {x = 266.09, y = -349.35, z = 44.74, model = "prop_sec_barier_02b"},
     {x = 285.28, y = -355.78, z = 45.13, model = "prop_sec_barier_02a"},
+    -- Gabz Casino
+    {x = 1036.43, y = 51.77, z = 69.06, model = "vw_prop_casino_blckjack_01"},
+    {x = 1044.12, y = 53.47, z = 69.06, model = "vw_prop_casino_blckjack_01"},
+    {x = 1043.52, y = 56.33, z = 69.06, model = "vw_prop_casino_blckjack_01"},
+    {x = 1038.22, y = 55.68, z = 69.06, model = "vw_prop_casino_blckjack_01"},
+    {x = 1025.9, y = 61.5, z = 69.87, model = "vw_prop_casino_blckjack_01b"},
+    {x = 1030.79, y = 40.68, z = 69.87, model = "vw_prop_casino_blckjack_01b"},
+    {x = 1025.8, y = 47.74, z = 68.86, model = "vw_prop_casino_roulette_01"},
 }
 
 CreateThread(function()
